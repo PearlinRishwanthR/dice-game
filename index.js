c9200d25c68410ac1d66e4e5706ad7533455e882
pearlin rishwanth <pearlinrishwanth1@gmail.com>
2023-03-17 11:32:00 +0530

    ji

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var randomNumber1 =Math.floor(Math.random()*6+1);
 
 var randomNumber2 =Math.floor(Math.random()*6+1);
 
-
 document.querySelector(".img1").setAttribute('src',"images/dice"+randomNumber1+".png");
 document.querySelector(".img2").setAttribute('src',"images/dice"+randomNumber2+".png");
 console.log(randomNumber1);
