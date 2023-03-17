a7c1d2dc98db9a6ea18a76f93c56e26d5e7198a3
pearlin rishwanth <pearlinrishwanth1@gmail.com>
2023-03-17 11:31:20 +0530

    dice game

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 =Math.floor(Math.random()*6+1);
+
+
+var randomNumber2 =Math.floor(Math.random()*6+1);
+
+
+document.querySelector(".img1").setAttribute('src',"images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute('src',"images/dice"+randomNumber2+".png");
+console.log(randomNumber1);
+console.log(randomNumber2);
+
+
+if(randomNumber1==randomNumber2){
+    document.querySelector("h1").textContent="Draw";
+} else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Player 1 Wins";
+}else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent="Player 2 Wins";
+}
